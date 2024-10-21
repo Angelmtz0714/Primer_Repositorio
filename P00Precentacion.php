1c5eebb80496e5b845d1641593e7ed7f89ab2b30
Angelmtz0714 <Angelmtz1428@gmail.com>
2024-10-21 11:31:33 -0400

    Add files via upload

---
+++ P00Precentacion.php
@@ -0,0 +1,23 @@
+<?php
+
+
+
+$N="Martinez Contreras Angel";
+$E="16";
+$G="A";
+$R= "Durango, Dgo";
+$O= "Programacion";
+
+echo "Mi nombre es" , $N;
+echo "<br>";
+echo "Mi edad es" , $E;
+echo "<br>";
+echo "Mi grupo es" , $G;
+echo "<br>";
+echo "Mi ciudad es ", $R;
+echo "<br>";
+echo "Mi carrera es ", $O;
+echo "<br>";
+
+
+?>
